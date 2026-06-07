fba1a1d62eb7dc0239d14dbf2cca26a2c4155f86
rikimaru35kg <rikimaru35kg@gmail.com>
2026-06-07 16:26:02 +0900

    [new] poweshell aliases

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,15 @@
+Set-Alias ll Get-ChildItem
+Set-Alias grep Select-String
+function touch { New-Item $args -ItemType File }
+function gs { git status }
+function ga { git add -A }
+function gc { git commit -m $args }
+function gp { git push }
+function which { Get-Command $args }
+function tree { Get-ChildItem -Recurse }
+function find {
+  param([string]$path = ".",
+        [string]$name = "*")
+  Get-ChildItem -Path $path -Recurse -Filter $name
+}
+
